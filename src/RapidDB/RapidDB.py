b96d9f5b510f249d08b73ca16bc9d805f73b5053
TN3W <tn3wA8xxfuVMs2@proton.me>
2023-10-02 13:55:04 +0000

    Added JSON class

---
+++ src/RapidDB/RapidDB.py
@@ -0,0 +1,45 @@
+import os
+import json
+from threading import Lock
+from typing import Union
+
+file_locks = dict()
+
+class JSON:
+
+    def load(file_name: str) -> Union[dict, list]:
+        """
+        Function to load a JSON file securely.
+
+        :param file_name: The JSON file you want to load
+        """
+
+        if not os.path.isfile(file_name):
+            raise FileNotFoundError("File '" + file_name + "' does not exist.")
+        
+        if file_name not in file_locks:
+            file_locks[file_name] = Lock()
+
+        with file_locks[file_name]:
+            with open(file_name, "r") as file:
+                data = json.load(file)
+            return data
+        
+    def dump(data: Union[dict, list], file_name: str) -> None:
+        """
+        Function to save a JSON file securely.
+        
+        :param data: The data to be stored should be either dict or list
+        :param file_name: The file to save to
+        """
+
+        file_directory = os.path.dirname(file_name)
+        if not os.path.isdir(file_directory):
+            raise FileNotFoundError("Directory '" + file_directory + "' does not exist.")
+        
+        if file_name not in file_locks:
+            file_locks[file_name] = Lock()
+
+        with file_locks[file_name]:
+            with open(file_name, "w") as file:
+                json.dump(data, file)
